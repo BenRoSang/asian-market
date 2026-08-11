08a1e58f1f6d35b2816937771b01a76d5b01626a
Zar Hlei Sang <santisang8@gmail.com>
2026-08-11 22:54:35 +0200

    initail project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Welcome to My Small Shop
+      </h1>
+    </main>
+  );
+}
+
+
+export default App
